6ef7c466ef3596fda45e697197a5de09d6fa69bd
Serhat Ozturk <so2turk@yahoo.com>
2021-09-19 16:37:20 +0200

    Implement docker

--- src/database-connection.js
+++ src/database-connection.js
@@ -13,15 +13,3 @@ mongoose
   })
   .then(() => console.log('connection established'))
   .catch(console.log)
-
-// const Panda = mongoose.model('Panda', { name: String, age: Number })
-
-// async function main() {
-//   const pandas = await Panda.find({ age: { $gte: 18, $lte: 21 }, name: 'anna' })
-//   console.log(pandas)
-// }
-
-// main()
-
-// const panda = new Panda({ name: 'anna', age: 19 })
-// panda.save().then(() => console.log(`we have a new panda, ${panda.name}!`))
