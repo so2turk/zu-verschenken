b49979901427413d7646becc7bbb10abccbdd703
Serhat Ozturk <so2turk@yahoo.com>
2021-09-22 21:05:49 +0200

    Install vue-cli and create frontend folder

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .home
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
+    div(v-for="main in mains") {{ main.name }} has {{ main.present.length }} gifts
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      mains: [],
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/mains')
+
+    this.mains = usersRequest.data
+  },
+}
+</script>
