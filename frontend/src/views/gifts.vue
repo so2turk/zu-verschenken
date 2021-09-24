788c8d34df7a3d9557958e26a255fec716c9cd1e
Serhat Ozturk <so2turk@yahoo.com>
2021-09-24 14:15:46 +0200

    Create user and gift view files and respective componens

---
+++ frontend/src/views/gifts.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios'
+import GiftCard from '@/components/gift-card.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    GiftCard,
+  },
+  data() {
+    return {
+      gifts: [],
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/gifts')
+    this.gifts = usersRequest.data
+  },
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 Gifts
+    gift-card(v-for="gift in gifts" :gift="gift")
+</template>
---
+++ frontend/src/views/users.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios'
+import UserCard from '@/components/user-card.vue'
+
+export default {
+  name: 'Users',
+  components: {
+    UserCard,
+  },
+  data() {
+    return {
+      mains: [],
+      gifts: [],
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/mains')
+
+    this.mains = usersRequest.data
+  },
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 Users
+    user-card(v-for="main in mains" :main="main")
+</template>
