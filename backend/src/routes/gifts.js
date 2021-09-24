16d957aee196d6c6079ea24d064f12f868b016dd
Serhat Ozturk <so2turk@yahoo.com>
2021-09-24 14:18:14 +0200

    Add get listing functionality to gifts backend

--- backend/src/routes/gifts.js
+++ backend/src/routes/gifts.js
@@ -3,6 +3,21 @@ const express = require('express')
 const router = express.Router()
 const Gift = require('../models/gift')
 
+/* GET gifts listing. */
+router.get('/', async (req, res) => {
+  const query = {}
+
+  if (req.query.name) {
+    query.name = req.query.name
+  }
+
+  if (req.query.address) {
+    query.address = req.query.address
+  }
+
+  res.send(await Gift.find(query))
+})
+
 /* POST create a gift */
 router.post('/', async (req, res) => {
   const createdGift = await Gift.create(req.body)
