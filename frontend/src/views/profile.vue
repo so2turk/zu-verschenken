79276ca45359274bafaffa11cbed6cae91158377
Serhat Ozturk <so2turk@gmail.com>
2021-10-25 01:26:08 +0200

    Rearrange profile view file

--- frontend/src/views/profile.vue
+++ frontend/src/views/profile.vue
@@ -2,6 +2,7 @@
 import UserCard from '@/components/user-card.vue'
 import Counter from '@/components/counter.vue'
 import AddGiftCard from '@/components/add-gift-card.vue'
+
 import { mapActions, mapState } from 'vuex'
 
 export default {
@@ -33,26 +34,66 @@ export default {
 
 <template lang="pug">
   .home
-    h1 {{ user.name }}
-    p The time is: {{ time }}
-    p
-    UserCard(:user="user" v-if="user")
-    p
-    div(v-if="liveStreams.length")
-      h2 Live streams
-      div(v-for="stream in liveStreams")
-        p {{ stream }}
-        button(@click="joinStream(stream)") Join stream
-    button(@click="goLive") Go live
-    div(v-if="currentLiveStream")
-      h3 Live stream
-      .messages
-        .message(v-for="message in liveStreamMessages")
-          p
-            span {{ message.author }}:&nbsp;
-            span {{ message.body }}
-      form(@submit="sendMessage")
-        input(type="text" v-model="message")
-        input(type="submit" value="Send message")
+    .box
+      .div
+        .row-1
+          h1 {{ user.name }} <br>
+        .row-2
+          .col-2
+            UserCard(:user="user" v-if="user")
+          .col-2
             AddGiftCard
+        .row-3
+          p The time is: {{ time }}
+          div(v-if="liveStreams.length")
+            h2 Live streams
+            div(v-for="stream in liveStreams")
+              p {{ stream }}
+              button(@click="joinStream(stream)") Join stream
+          button(@click="goLive") Go live
+          div(v-if="currentLiveStream")
+            h3 Live stream
+            .messages
+              .message(v-for="message in liveStreamMessages")
+                p
+                  span {{ message.author }}:&nbsp;
+                  span {{ message.body }}
+            form(@submit="sendMessage")
+              input(type="text" v-model="message")
+              input(type="submit" value="Send message")
 </template>
+
+
+<style lang="scss" scoped>
+.row-1 {
+  height: 90%; 
+  width:100%;
+  display:flex;
+  align-items: center;
+  // justify-content: center;
+}
+.row-1 {
+  height: 50px;
+  border: 1px solid #c80;
+  display: flex;
+  flex-grow: 1;
+  background-color: #dd5;
+  margin: 2px;
+}
+.row-2 {
+  border: 1px solid #c80;
+  display: flex;
+  flex-grow: 1;
+  background-color: #dd5;
+  margin: 2px;
+}
+.col-1 {
+  background: #dd5;
+}
+.col-2 {
+  background: #dd5;
+  flex-grow: 1;
+  padding: 1px;
+}
+
+</style>
