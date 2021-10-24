cecc8cbe9f68c86fc5881933f2374f05efcbc673
Serhat Ozturk <so2turk@gmail.com>
2021-10-25 01:50:44 +0200

    Remove interestIn functionality from about file

--- frontend/src/views/about.vue
+++ frontend/src/views/about.vue
@@ -1,11 +1,9 @@
 <script>
-import InterestedIn from '@/components/interested-in.vue'
-
 export default {
   data() {
     return {}
   },
-  components: { InterestedIn },
+  components: {},
   methods: {},
 }
 </script>
@@ -15,6 +13,5 @@ export default {
     h1 zu Verschenken
     p a platform to share what we don't need
     img(src="../assets/csm_Berlin-c-Shutterstock-2640_f98e087001.jpg")
-    InterestedIn
 
 </template>
