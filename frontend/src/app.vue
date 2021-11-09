b82d7446991e1aaf7accbef26a7fa335a8ff2f1a
Serhat Ozturk <so2turk@gmail.com>
2021-11-09 17:37:36 +0100

    Introduce new styling to app.vue

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -26,9 +26,9 @@ export default {
 }
 </script>
 <template lang="pug">
-  #app
-    .row-2
-      .col-2
+  #nav
+    .row-1
+      .col-1
         router-link(to="/")
           img(v-if="dark" alt="zuV logo" width="100" src="../src/assets/logoBig-b.png")
           img(v-else alt="zuV logo" width="100" src="../src/assets/logoBig.png")        
@@ -49,22 +49,86 @@ export default {
           router-link(to="/map" v-if="user") Map |&nbsp;
           router-link(to="/add-new-gift" v-if="user") Add New Gift
       .col-2
+
+      .col-6
+        router-view
+      .col-5
+        p
+        p
+        <br><br>
         Search
-      .col-2
-        div(v-if="!dark")
-          img(@click="switchMode()" width="25px" src="https://d29fhpw069ctt2.cloudfront.net/icon/image/38007/preview.svg")
-        div(v-else)
-          img(@click="switchMode()" width="40px" src="https://d29fhpw069ctt2.cloudfront.net/icon/image/49197/preview.svg")
-    router-view
-    #footer 
-      p
-      p
-      footer.footer 
-        div 
-          span.text-muted (c) SO 2021
+    .row-3
+      #footer
+        footer.footer 
+          div 
+            span.text-muted (c) SO 2021
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  font-family: Arial;
+}
+
+a {
+  text-decoration: none;
+}
+
+#page-wrap {
+  margin: auto;
+  max-width: 850px;
+  min-height: 100vh;
+}
+
+#nav {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+
+}
+
+.row-1 {
+  display: flex;
+  flex-grow: 1;
+  margin: 2px;
+  width: 100%;
+  align-self: flex-start;
+}
+.col-1 {
+  width: 100px;
+  align-self: flex-start;
+}
+.col-2 {
+  text-align: left;
+  flex-grow: 3;
+  font-size: 22px;
+  flex-direction:row-reverse;
+}
+.col-3 {
+  width: 200px;
+  align-self: center;
+  align-content:space-around
+}
+.col-4 {
+  width: 40px;
+  align-self: center;
+}
+.row-2 {
+  display: flex;
+  flex-grow: 1;
+  margin: 2px;
+  width: 100%;
+}
+.col-5 {
+  align-self: stretch;
+  font-size: 18px;
+}
+.col-6 {
+  flex-grow: 3;
+  align-self: center;
+  font-size: 22px;
+}
+
 body {
   --text-color: #222;
   --bkg-color: #fff;
@@ -91,37 +155,12 @@ body { background: var(--bkg-color); }
 h1,
 p { color: var(--text-color); }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  align-content: center;
 }
 
-.col-2 {
-  flex-grow: 1;
-  padding: 2px;
 }
 
-.row-2 {
-  //border: 1px solid #c80;
-  display: flex;
-  flex-grow: 1;
-  margin: 2px;
-  width: 800px;
 }
 
-#nav {
-  padding: 3px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
