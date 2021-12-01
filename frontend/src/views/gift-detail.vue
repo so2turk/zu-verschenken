a138288cd2b6af86cbb95e6e882172f6208f095d
Serhat Ozturk <so2turk@gmail.com>
2021-12-01 22:08:56 +0100

    Remove style/googlemaps component and add page wrap

--- frontend/src/views/gift-detail.vue
+++ frontend/src/views/gift-detail.vue
@@ -1,13 +1,11 @@
 <script>
-import GiftCard from '@/components/gift-card.vue'
+import GiftDetailCard from '@/components/gift-detail-card.vue'
 import CommentCard from '@/components/comment-card.vue'
-import GoogleMaps from '@/components/google-maps.vue'
-
 import { mapActions } from 'vuex'
 
 export default {
   name: 'GiftDetail',
-  components: { GiftCard, CommentCard, GoogleMaps },
+  components: { GiftDetailCard, CommentCard },
   data() {
     return {
       gift: null,
@@ -27,51 +25,10 @@ export default {
 </script>
 
 <template lang="pug">
-  .GiftDetail
-    GiftCard(:gift="gift" v-if="gift")
-    .box
-      .row-2
-        .col-2
-          CommentCard(:gift="gift" v-if="gift" @addComment="updateGift()")
-        .col-1
-          GoogleMaps(:gift="gift" v-if="gift")
+  .GiftDetail(id="page-wrap")
+    .row-1
+      GiftDetailCard(:gift="gift" v-if="gift" @anyChange="updateGift()")
+    .row-1
+      .col-6
+        CommentCard(:gift="gift" v-if="gift" @addComment="updateGift()")
 </template>
-
-<style lang="scss" scoped>
-label {
-  display: block;
-  margin: 1rem 0;
-}
-
-.box {
-  padding: 1rem;
-  width: 800px;
-  border: 1px solid rgb(39, 61, 4);
-  background-color: #dd0;
-  border-radius: 0.3rem;
-}
-
-.row-1 {
-  height: 50px;
-  // border: 1px solid #c80;
-  display: flex;
-  flex-grow: 1;
-  background-color: #dd5;
-  margin: 2px;
-}
-.row-2 {
-  //border: 1px solid #c80;
-  display: flex;
-  flex-grow: 1;
-  background-color: #dd5;
-  margin: 2px;
-}
-.col-1 {
-  background: #dd5;
-}
-.col-2 {
-  background: #dd5;
-  flex-grow: 0;
-  padding: 2px;
-}
-</style>
