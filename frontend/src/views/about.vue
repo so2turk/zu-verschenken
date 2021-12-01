fb7dfd17a3ed027c7e4f66f44156749e6d1a48a0
Serhat Ozturk <so2turk@gmail.com>
2021-12-01 21:01:56 +0100

    Add info about zuV and page wrap to about page

--- frontend/src/views/about.vue
+++ frontend/src/views/about.vue
@@ -9,9 +9,23 @@ export default {
 </script>
 
 <template lang="pug">
-  .about
+  .about(id="page-wrap")
     h1 zu Verschenken
     p a platform to share what we don't need
-    img(src="../assets/csm_Berlin-c-Shutterstock-2640_f98e087001.jpg")
+    img(width=850 src="https://www.falstaff.de/fileadmin/_processed_/7/b/csm_Berlin-c-Shutterstock-2640_f98e087001.jpg")
+    p The idea seems to be similar to free-your-stuff.com (fys).. but zuV mainly concentrates on things left in front of doors/buildings to be found/taken..
+      | Actually idea comes from a real life experience.. as I’ve various pieces at home that I gathered while I walk along the neighborhood.. including toys.. 
+      | lamp.. even small bookshelf.. unfortunately fys does not cover streets (may though.. never contacted them).. 
+      | and usually things left outside degrade before someone who is in need finds them.. 
+      | that’s why I thought if there would be a platform that covers those gifts (yeah I believe they are gifts) 
+      | it would be much beneficial not only for those who are in need but also for everyone as this would help economy/environment.. 
+      | anyway.. idea is named as ‘zu Verschenken’ as our heroes (givers) usually leaves this note with the gifts.. 
+      | in order to use platform, since the idea is getting those lonely gifts with their owners (lucky ones) together.. 
+      | subscription won’t be needed unless you want to get notified or add gifts.. since every hero wants to stay anonymous.. 
+      | gift post will also be done anonymously.. additionally, any user will be allowed to post gifts of other superheroes who are not using our platform.. 
+      | in order to find whether you are luck or not.. searching through district.. category.. (and maybe near me options) will be available.. 
+      | the only thing that will be asked from any user could be an update.. so as no one would be disappointed with already gone/degraded gifts.. 
+      | but in any case every gift will be deleted/archived in a couple of days (or will be presented with warning).. 
+      | for the future.. platform could be enlarged with sharing option.. and if enough data gathered users can be fed with stats for their 'waiting or buying' decisions..
 
 </template>
