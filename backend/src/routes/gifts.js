4a2af1ee9917f0ac2d4d0e70578925c652989f48
Serhat Ozturk <so2turk@gmail.com>
2021-12-01 23:42:16 +0100

    Add del-by-id and delgift (with connected data within other object) methods

--- backend/src/routes/gifts.js
+++ backend/src/routes/gifts.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const axios = require('axios')
 
 const Gift = require('../models/gift')
+const Photo = require('../models/photo')
 
 /* GET gifts listing. */
 router.get('/', async (req, res) => {
@@ -31,6 +32,39 @@ router.get('/:giftId', async (req, res) => {
 
   if (gift) res.send(gift)
   else res.sendStatus(404)
+})
+
+router.delete('/:giftId', async (req, res) => {
+  const gift = await Gift.findByIdAndDelete(req.params.giftId)
+
+  if (gift) res.send(gift)
+  else res.sendStatus(404)
+})
+
+router.post('/delGift', async (req, res) => {
+  const gift = await Gift.findById(req.body.giftId)
+
+  for(let i=0; i<gift.interestBy.length; i++){
+    await gift.delInterest(gift.interestBy[i])
+  }
+  for(let i=0; i<gift.acceptBy.length; i++){
+    await gift.delTake(gift.acceptBy[i])
+  }
+  for(let i=0; i<gift.comments.length; i++){
+    await gift.delComment(gift.comments[i])
+  }
+  const ID = ''
+  for(let i=0; i<gift.photos.length; i++){
+    await axios.delete("https://api.imgur.com/3/image/" + gift.photos[i].del,{
+    method: "delete", headers: { Authorization: `Client-ID ${ID}` },
+    })
+    await Photo.findByIdAndDelete(gift.photos[i]._id)
+  }
+
+  await Gift.findByIdAndDelete(req.body.giftId)
+  await gift.delPresent(gift.presentBy)
+
+  res.status(200).send(gift)
 })
 
 router.get('/:giftId/json', async (req, res) => {
